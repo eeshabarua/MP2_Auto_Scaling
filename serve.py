c26ac754adfaeb6fef334ef3902e1cc5ff690cce
eeshabarua <eesha.barua@gmail.com>
2024-01-25 13:50:11 -0600

    Add POST and GET

--- serve.py
+++ serve.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import requests
+import subprocess
+import socket
+import sys
 
 app = Flask(__name__)
 
-seed = 100
-
 @app.route('/', methods=["POST"])
 def POST_num():
+
+    command = [sys.executable, "stress_cpu.py"]
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    stdout, stderr = process.communicate()
+    print(stdout, stderr)
     return jsonify({'result': 'success'})
 
 @app.route('/', methods=["GET"])
 def GET_num():
-    return "success", 200
+    name = socket.gethostname()
+    return str(name), 200
+
---
+++ test.py
@@ -0,0 +1,1 @@
+print("Hellow :3")
